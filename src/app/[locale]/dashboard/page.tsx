6c1c7dba37e9632c365944e2a9ab4d4fed2cdd8d
Ege <orhanebilge07@gmail.com>
2024-07-01 23:06:33 +0300

    feat: add company selection dropdown

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,74 +1,86 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Calender, CheckList, Clock, EuroCoin, Overview, Percent, Loan, PiggyCoin } from '@/icons/dashboard'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import FilterComponent from '@/modules/dashboard/filter-component'
 import DynamicFilter from '@/modules/dashboard/filters/dynamic-filter'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import Projects from '@/modules/dashboard/projects'
 import DataTable from '@/modules/dashboard/datatable'
+import Projects from '@/modules/dashboard/projects'
+import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from '@/components/ui/select'
+
+const projectsData = [
+  {
+    title: 'Transfer Bonus',
+    badges: [
+      {
+        label: 'Marketing',
+        active: true,
+      },
+      { label: 'Design' },
+      { label: 'Coaching' },
+    ],
+    description:
+      'Die erstmalige Anschaffung von IT-Hardware und Software zur digitalen Transformation von Arbeits-, Produktions- und Managementprozessen, zur Einführung oder Verbesserung der betrieblichen IT-Sicherheit, digitalisierungsbezogene Beratungs- und Qualifizierungsmaßnahmen.',
+    statistics: [
+      {
+        value: '4,5%',
+        icon: <Percent />,
+      },
+      {
+        value: '500.000',
+        icon: <EuroCoin />,
+      },
+      {
+        value: '3J',
+        icon: <Calender />,
+      },
+      {
+        value: '10J',
+        icon: <Clock />,
+      },
+    ],
+    progress: 76,
+    icon: <PiggyCoin />,
+  },
+  {
+    title: 'Loan Card Title',
+    badges: [{ label: 'Loan Badge' }, { label: 'Another Badge' }],
+    description: 'Description for the loan card.',
+    statistics: [
+      {
+        value: '2.5%',
+        icon: <Percent />,
+      },
+      {
+        value: '750.000',
+        icon: <EuroCoin />,
+      },
+      {
+        value: '5J',
+        icon: <Calender />,
+      },
+      {
+        value: '15J',
+        icon: <Clock />,
+      },
+    ],
+    progress: 50,
+    icon: <Loan />,
+  },
+]
+
+const companies: { id: number; name: string }[] = [
+  { id: 1, name: 'Company A' },
+  { id: 2, name: 'Company B' },
+  { id: 3, name: 'Company C' },
+]
 
 export default function Dashboard() {
-  // Usage example
-  const projectsData = [
-    {
-      title: 'Transfer Bonus',
-      badges: [
-        {
-          label: 'Marketing',
-          active: true,
-        },
-        { label: 'Design' },
-        { label: 'Coaching' },
-      ],
-      description:
-        'Die erstmalige Anschaffung von IT-Hardware und Software zur digitalen Transformation von Arbeits-, Produktions- und Managementprozessen, zur Einführung oder Verbesserung der betrieblichen IT-Sicherheit, digitalisierungsbezogene Beratungs- und Qualifizierungsmaßnahmen.',
-      statistics: [
-        {
-          value: '4,5%',
-          icon: <Percent />,
-        },
-        {
-          value: '500.000',
-          icon: <EuroCoin />,
-        },
-        {
-          value: '3J',
-          icon: <Calender />,
-        },
-        {
-          value: '10J',
-          icon: <Clock />,
-        },
-      ],
-      progress: 76,
-      icon: <PiggyCoin />,
-    },
-    {
-      title: 'Loan Card Title',
-      badges: [{ label: 'Loan Badge' }, { label: 'Another Badge' }],
-      description: 'Description for the loan card.',
-      statistics: [
-        {
-          value: '2.5%',
-          icon: <Percent />,
-        },
-        {
-          value: '750.000',
-          icon: <EuroCoin />,
-        },
-        {
-          value: '5J',
-          icon: <Calender />,
-        },
-        {
-          value: '15J',
-          icon: <Clock />,
-        },
-      ],
-      progress: 50,
-      icon: <Loan />,
-    },
-  ]
+  const [selectedCompany, setSelectedCompany] = useState(companies[0].id)
+
+  const handleCompanyChange = (value: string): void => {
+    setSelectedCompany(parseInt(value))
+  }
 
   return (
     <>
@@ -95,22 +107,33 @@ export default function Dashboard() {
         <TabsContent
           value="overview"
           className="p-3 rounded"
-          style={{
-            boxShadow: '0px 8px 32px rgba(56, 71, 109, 0.14)',
-          }}
+          style={{ boxShadow: '0px 8px 32px rgba(56, 71, 109, 0.14)' }}
         >
           <div className="px-3 pb-2">
             <FilterComponent />
             <DynamicFilter />
+            <div className="my-4 w-48">
+              <Select onValueChange={handleCompanyChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Company" />
+                </SelectTrigger>
+                <SelectContent>
+                  {companies.map(company => (
+                    <SelectItem key={company.id} value={company.id.toString()}>
+                      <div className="flex justify-between items-center w-full">{company.name}</div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
           </div>
           <DataTable />
         </TabsContent>
+
         <TabsContent
           value="byproject"
           className="p-3 rounded"
-          style={{
-            boxShadow: '0px 8px 32px rgba(56, 71, 109, 0.14)',
-          }}
+          style={{ boxShadow: '0px 8px 32px rgba(56, 71, 109, 0.14)' }}
         >
           <div className="px-3 pb-2">
             <FilterComponent />
@@ -119,9 +142,7 @@ export default function Dashboard() {
           <Tabs
             defaultValue="transfer bonus"
             className="w-full md:flex"
-            style={{
-              filter: 'drop-shadow(11.66px 11.66px 23.32px rgba(167, 167, 167, 0.15))',
-            }}
+            style={{ filter: 'drop-shadow(11.66px 11.66px 23.32px rgba(167, 167, 167, 0.15))' }}
           >
             <TabsList className="flex justify-start md:flex-col h-full bg-transparent">
               {projectsData.map((tab, index) => (
